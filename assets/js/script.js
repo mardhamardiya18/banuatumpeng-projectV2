1f9cb5582ebaf96ddd0b174ccea00d89821c121c
mardhamardiya.18 <mardhamardiya.18@gmail.com>
2021-10-06 16:05:48 +0700

    update respond

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,44 @@ var swiper = new Swiper(".mySwiperBlog", {
         prevEl: ".bxs-chevron-left-circle",
     },
 });
+
+$(document).ready(function () {
+    $('.nav-link').on('click', function () {
+        $('.nav-link').removeClass('active')
+        $(this).addClass('active')
+    })
+
+})
+
+$(window).on("scroll", function (e) {
+    if ($(window).scrollTop() > 70) {
+        $(".navbar").addClass("show");
+    } else {
+        $(".navbar").removeClass("show");
+    }
+    e.preventDefault()
+});
+
+// // app script Spreadsheet
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwcgiP-InGYrNUJfEZcLxrvt4T1WTwkEiGdiYs2dYh8odbTr4zXyc73vGZ-6NyFUNNV3g/exec'
+const form = document.forms['subscribe']
+const done = document.querySelector('.clicked-subscribe')
+const btnSent = document.querySelector('.btn__subscribe-done')
+const btnSend = document.querySelector('.btn-subs')
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    btnSend.classList.toggle('d-none')
+    btnSent.classList.toggle('d-none')
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            done.classList.toggle('d-none')
+            form.classList.toggle('d-none')
+            form.reset()
+            console.log('Success!', response)
+        })
+
+        .catch(error => console.error('Error!', error.message))
+})
+
